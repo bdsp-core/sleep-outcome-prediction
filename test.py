a1ab2272622f78f07eef21288eca5212f7a17bb2
EC2 Default User <ec2-user@ip-172-16-66-241.ec2.internal>
2022-10-19 19:50:53 +0000

    temp push, in case BDSP shuts down

--- test.py
+++ test.py
@@ -1,6 +1,35 @@
 import os
-import h5py
+import pandas as pd
 
-with h5py.File('../BAI-EMU/0a0c9ac72a7577ece4de9448bb8178b04c08f5d45941d27375097a69d23419bd_20210524_141512/0a0c9ac72a7577ece4de9448bb8178b04c08f5d45941d27375097a69d23419bd_0_20210524_141512.mat', 'r') as ff:
-    print(ff.keys())
-    print(ff['data'].shape)
+
+def read_psg_from_bdsp(sid, dov):
+    """
+    sid is a string, HashID
+    dov is a string, shifted date in YYYMMDD format
+    """
+    base_folder = '/home/ec2-user/studies/BDSP-CORE/PSG/data/S0001'
+    data_folders = os.listdir(base_folder)
+
+    data_folder = [x for x in data_folders if f'{sid}_{dov}' in x]
+    import pdb;pdb.set_trace()
+    assert len(data_folder)==1
+    data_folder = data_folder[0]
+    data_files = os.listdir(os.path.join(base_folder, data_folder))
+    signal_path = o
+
+
+def main():
+    # read mastersheet
+    df = pd.read_excel('mastersheet_outcome_deid.xlsx')
+    print(df)
+
+    # specify which subject you need
+
+    i = 0
+    sid = df.HashID.iloc[i]
+    dov = df.DOVshifted.iloc[i].strftime('%Y%m%d')
+
+
+if __name__=='__main__':
+    main()
+
